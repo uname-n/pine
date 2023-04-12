c4b98dea37957b04b20e44305ff105e2316595f7
uname-n <git@defend.sh>
2023-04-11 20:52:57 -0700

    base

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::fs::{File, create_dir_all};
+use std::fs::{File, create_dir_all, read_dir, remove_file};
 use std::io::{Write, Read};
 use std::path::PathBuf;
 use bincode::{serialize, deserialize};
-
 use ndarray::Array1;
-
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Vector {
     id: String,
     data: Array1<f32>,
@@ -21,137 +19,156 @@ impl Vector {
 
 #[derive(Debug)]
 struct Pine {
-    path:PathBuf,
+    path: PathBuf,
+    chunk_similarity: f32,
 }
 
 impl Pine {
-    fn new(path:PathBuf) -> Pine {
-        create_dir_all(&path).expect("failed to create directory");
-        Pine { path }
-    }
-    
-    fn save(&self, vector:&Vector) {
-        let path = self.path.join(&vector.id);
-        let serialized = serialize(&vector).expect("failed to serialize");
-        let mut file = File::create(path).expect("failed to create file");
-        file.write_all(&serialized).expect("failed to write file");
-    }
-    
-    fn load(&self, id:&String) -> Option<Vector> {
-        let path = self.path.join(&id);
-        if !path.exists() { None } else {
-            let mut file = File::open(path).expect("failed to open file");
-            let mut buffer = Vec::new();
-            file.read_to_end(&mut buffer).expect("failed to read file");
-            let vector:Vector = deserialize(&buffer).expect("failed to deserialize");
-            Some(vector)
-        }
+    fn new(path: PathBuf, chunk_similarity: f32) -> Pine {
+        create_dir_all(&path.join("vectors")).expect("failed to create directory");
+        create_dir_all(&path.join("index")).expect("failed to create directory");
+        
+        return Pine { path, chunk_similarity };
     }
 
-    fn remove(&self, id:&String) {
-        let path = self.path.join(&id);
-        if path.exists() {
-            std::fs::remove_file(path).expect("failed to remove file");
+    fn get_index(&self, id:&String) -> Option<PathBuf> {
+        let index_path = self.path.join("index").join(id);
+        if index_path.exists() {
+            let mut file = File::open(index_path).expect("failed to open file");
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer).expect("failed to read file");
+            let path = PathBuf::from(String::from_utf8(buffer).expect("failed to convert to string"));
+            return Some(path.join(id));
+        }
+        return None;
+    }
+
+    fn set_index(&self, id:&String, path:&PathBuf) {
+        let mut file = File::create(self.path.join("index").join(id)).expect("failed to create file");
+        file.write_all(path.to_str().unwrap().as_bytes()).expect("failed to write file");
+    }
+
+    fn find_dir(&self, vector:&Vector) -> Option<PathBuf> {
+        for entry in read_dir(self.path.join("vectors")).expect("failed to read directory") {
+            let path = entry.expect("failed to read directory").path();
+            
+            if path.is_dir() {
+                let metadata_path = path.join("metadata"); 
+                if metadata_path.exists() {
+                    let mut file = File::open(metadata_path).expect("failed to open file");
+                    let mut buffer = Vec::new();
+                    file.read_to_end(&mut buffer).expect("failed to read file");
+                    let metadata: Vector = deserialize(&buffer).expect("failed to deserialize vector");
+
+                    if self.cosine_similarity(&vector, &metadata) > self.chunk_similarity {
+                        return Some(path); 
+                    }
+                }
+            }
+        }
+        return None;
+    }
+
+    fn save(&self, vector:&Vector) {
+        let serialized = serialize(&vector).expect("failed to serialize vector");
+        
+        let path = self.find_dir(&vector).unwrap_or_else(|| {
+            let current_size = read_dir(&self.path.join("vectors")).expect("failed to read directory").count();
+            let path = self.path.join("vectors").join(format!("{:03}", current_size));
+            create_dir_all(&path).expect("failed to create directory");
+
+            let mut file = File::create(path.join("metadata")).expect("failed to create file");
+            file.write_all(&serialized).expect("failed to write file");
+            
+            return path;
+        });
+
+        self.delete(&vector.id);
+
+        let mut file = File::create(path.join(&vector.id)).expect("failed to create file");
+        file.write_all(&serialized).expect("failed to write file");
+
+        self.set_index(&vector.id, &path);
+    }
+
+    fn load(&self, id:&String) -> Option<Vector> {
+        let path = self.get_index(&id);
+        if path.is_some() {
+            let mut file = File::open(path.unwrap()).expect("failed to open file");
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer).expect("failed to read file");
+            let vector: Vector = deserialize(&buffer).expect("failed to deserialize vector");
+            return Some(vector);
+        }
+        return None;
+    }
+
+    fn delete(&self, id:&String) {
+        let path = self.get_index(&id);
+        if path.is_some() {
+            remove_file(path.unwrap()).expect("failed to delete file");
         }
     }
 
     fn exists(&self, id:&String) -> bool {
-        let path = self.path.join(&id);
-        path.exists()
+        return self.get_index(&id).is_some();
     }
 
     fn size(&self) -> usize {
-        std::fs::read_dir(&self.path).expect("failed to read directory").count()
-    }
-
-    fn clear(&self) {
-        for entry in std::fs::read_dir(&self.path).expect("failed to read directory") {
-            if let Ok(entry) = entry {
-                let path = entry.path();
-                std::fs::remove_file(path).expect("failed to remove file");
+        let mut size = 0;
+        for entry in read_dir(self.path.join("vectors")).expect("failed to read directory") {
+            let path = entry.expect("failed to read directory").path();
+            
+            if path.is_dir() {
+                for entry in read_dir(path).expect("failed to read directory") {
+                    let path = entry.expect("failed to read directory").path();
+                    if path.is_file() {
+                        size += 1;
+                    }
+                }
             }
         }
+        return size;
     }
 
-    fn distance(query_vector:&Vector, stored_vector:&Vector) -> f32 {
+    fn distance(&self, query_vector:&Vector, stored_vector:&Vector) -> f32 {
         let diff = &query_vector.data - &stored_vector.data;
         let squared_diff = diff.mapv(|x| x * x);
-        squared_diff.sum().sqrt() 
+        return squared_diff.sum().sqrt();
     }
 
-    fn cosine_similarity(query_vector:&Vector, stored_vector:&Vector) -> f32 {
+    fn cosine_similarity(&self, query_vector:&Vector, stored_vector:&Vector) -> f32 {
         let dot_product = query_vector.data.dot(&stored_vector.data);
         let query_norm = query_vector.data.dot(&query_vector.data).sqrt();
         let stored_norm = stored_vector.data.dot(&stored_vector.data).sqrt();
-        dot_product / (query_norm * stored_norm)
-    }
-    
-    fn query_nearest_neighbor(&self, query_vector:&Vector) -> Option<Vector> {
-        let mut max_similarity:f32 = f32::MIN;
-        let mut nearest_vector:Option<Vector> = None;
-
-        for entry in std::fs::read_dir(&self.path).expect("failed to read directory") {
-            if let Ok(entry) = entry {
-               let id = entry.file_name().into_string().expect("failed to convert file name");
-
-               if let Some(stored_vector) = self.load(&id) {
-                   let similarity = Pine::cosine_similarity(query_vector, &stored_vector);
-                   if similarity > max_similarity {
-                       max_similarity = similarity;
-                       nearest_vector = Some(stored_vector);
-                   }
-               }
-            }
-        }
-
-        nearest_vector
+        return dot_product / (query_norm * stored_norm);
     }
 }
 
 fn main() {
-    let pine = Pine::new(PathBuf::from("./vectors"));
-
-    let data = Array1::from(vec![1.0, 2.0, 3.0]);
-    let vector = Vector { id: "test".to_string(), data };
+    let pine = Pine::new(PathBuf::from("./pine"), 0.99);
 
     println!("pine: {:?}", pine);
-    println!("pine.path: {:?}", pine.path);
 
-    println!("vector: {:?}", vector);
-    println!("vector.id: {:?}", vector.id);
-    println!("vector.data: {:?}", vector.data);
-    println!("vector.size: {:?}", vector.size());
+    let abc = Vector { id: "abc".to_string(), data: Array1::from(vec![1.0, 2.0, -3.0, 4.0, -5.0]) };
+    pine.save(&abc);
 
-    pine.save(&vector);
+    println!("vector_size: {}", &abc.size());
+    
+    let xyx = Vector { id: "xyz".to_string(), data: Array1::from(vec![1.0, 2.0, -3.0, 4.0, -5.0]) };
+    pine.save(&xyx);
+    
+    for i in 0..20 {
+        let id = format!("{}", i);
+        let data = vec![i as f32 + 1.0, i as f32 + 2.0, i as f32 + 3.0, i as f32 + 4.0, i as f32 + 5.0];
+        pine.save(&Vector { id, data: Array1::from(data) });
+    }
+    
+    println!("size: {}", pine.size());
+    println!("exists: {}", pine.exists(&"abc".to_string()));
 
-    let load_vector = pine.load(&vector.id);
-    println!("load_vector:{:?}", load_vector);
-   
-    let a = Vector { id: "a".to_string(), data: Array1::from(vec![1.0, 2.0, 3.0]) };
-    let b = Vector { id: "b".to_string(), data: Array1::from(vec![2.0, 3.0, 4.0]) };
-    let c = Vector { id: "c".to_string(), data: Array1::from(vec![3.0, 4.0, 5.0]) };
+    println!("distance: {}", pine.distance(&abc, &xyx));
 
-    pine.save(&a);
-    pine.save(&b);
-    pine.save(&c);
-
-    let exists = pine.exists(&a.id);
-    println!("exists: {:?}", exists);
-
-    println!("size: {:?}", pine.size());
-
-    let distance = Pine::distance(&a, &b);
-    println!("distance: {:?}", distance);
-
-    let z = Vector { id: "z".to_string(), data: Array1::from(vec![5.0, 3.0, 4.0]) };
-    println!("z: {:?}", z);
-
-    let neighbor = pine.query_nearest_neighbor(&z);
-    println!("query_nearest_neighbor: {:?}", neighbor);
-
-    pine.remove(&a.id);
-    pine.remove(&b.id);
-    pine.remove(&c.id);
-
-    pine.clear();
+    let ghi = pine.load(&"abc".to_string()).unwrap();
+    println!("def: {:?}", ghi);
 }
